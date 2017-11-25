71d9f4a3fa714d6ac712b8d468aa1edcaa0535cd
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-11-25 14:09:40 -0600

    SchM_Update_V12
    Changes and Updates on the files .
    Disable the IndicatorLEDS of  Backgrown Task
    File: SchM.c , SchM.h

--- Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM.h
+++ Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM.h
@@ -1,25 +1,19 @@
-/*
- * SchM.h
- *
- *  Created on: 15/11/2017
- *      Author: uid87753
- */
-
 /*============================================================================*/
 /*                        I BS SOFTWARE GROUP                                 */
 /*============================================================================*/
 /*                        OBJECT SPECIFICATION                                */
 /*============================================================================*/
 /*!
- * $Source: filename.x $
+ * $Source: SchM.h  $
  * $Revision: 1 $
- * $Author: Guillermo Hernández $
- * $Date: 11/17/2017 $
+ * $Author: Hernandez Ramirez Guillermo, Hernandez Jimenez Manuel $
+ * $Date: 25/11/2017 $
  */
 /*============================================================================*/
 /* DESCRIPTION :                                                              */
-/**
- */
+/** \
+     Function Prototypes
+*/
 /*============================================================================*/
 /* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
 /* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
@@ -35,47 +29,55 @@
 /*============================================================================*/
 /*                    REUSE HISTORY - taken over from                         */
 /*============================================================================*/
-/*       AUTHOR        |      VERSION       |          DESCRIPTION            */
+/*  AUTHOR             |      VERSION       |        DESCRIPTION              */
 /*----------------------------------------------------------------------------*/
-/* Guillermo Hernández |           1        |        Update structures        */
-/*                     |                    |                                 */
-/* Manuel Hernández     |                    |                                 */
+/*Guillermo Hernandez  |          1    | Create the function Prototypes       */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
 /*
- * $Log: filename.h  $
- ============================================================================*/
+ * $Log: SchM.h  $
+  ============================================================================*/
 #ifndef BSW_SERVICES_SCHM_SCHM_H_
 #define BSW_SERVICES_SCHM_SCHM_H_
 
 /* Includes */
 /*============================================================================*/
-
 #include "Std_Types.h"
 #include "SchM_Cfg.h"
 #include "SchM_Types.h"
 
+
+/* Defines */
+/*============================================================================*/
+
+
 /* Constants and types */
 /*============================================================================*/
+
+typedef struct{
+    uint8_t FlagOverLoad: 1;    //Flag for indicating that an Overload have occured
+    uint8_t FlagTaskState:1;    //Flag for indicating that there is a task in Ready/Running state
+} Flags;
+
 
 /* Exported Variables */
 /*============================================================================*/
 
+
 /* Exported functions prototypes */
 /*============================================================================*/
 
-typedef struct {
-	uint8_t FlagOverLoad :1; //Flag for indicating that an Overload have occured
-	uint8_t FlagTaskState :1; //Flag for indicating that there is a task in Ready/Running state
-} Flags;
 
-extern void SchM_Init(const SchM_ConfigType *SchMConfig);
-extern void SchM_Start(void);
-extern void SchM_Stop(void);
+extern void SchM_Init( const SchM_ConfigType *SchMConfig );
+extern void SchM_Start( void );
+extern void SchM_Stop( void );
 void TurnOnOverloadPin(void);
 void TurnOnBackgroundPin(void);
 void TurnOffBackgroundPin(void);
 
 #endif /* BSW_SERVICES_SCHM_SCHM_H_ */
+ 
+
+/* Notice: the file ends with a blank new line to avoid compiler warnings */
 
