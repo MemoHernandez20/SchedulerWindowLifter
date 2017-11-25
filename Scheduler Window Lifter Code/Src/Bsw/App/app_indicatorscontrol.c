9cd7d347d16a99fd2a1c68d05dbefbb408a74dc0
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-11-25 12:42:33 -0600

    app_Indicatorscontrol_Update_V9
    Create "indicatorscontrol.c" & "indicatorscontrol.h" files on the APP layer . Include a function to control the status of the Indicators.
    File: app_indicatorscontrol.c , hal_indicatorscontrol.h

---
+++ Scheduler Window Lifter Code/Src/Bsw/App/app_indicatorscontrol.c
@@ -0,0 +1,94 @@
+/*============================================================================*/
+/*                        I BS SOFTWARE GROUP                                 */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*/
+/*!
+ * $Source: app_indicatorscontrol.c $
+ * $Revision: 1 $
+ * $Author: Hernandez Ramirez Guillermo, Hernandez Jimenez Manuel  $
+ * $Date: 25/11/2017 $
+ */
+/*============================================================================*/
+/* DESCRIPTION :                                                              */
+/** \file
+    APP file to control the status of the indicator LEDS
+
+*/
+/*============================================================================*/
+/* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
+/* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
+/* ALL RIGHTS RESERVED                                                        */
+/*                                                                            */
+/* The reproduction, transmission, or use of this document or its content is  */
+/* not permitted without express written authority. Offenders will be liable  */
+/* for damages.                                                               */
+/* All rights, including rights created by patent grant or registration of a  */
+/* utility model or design, are reserved.                                     */
+/*                                                                            */
+/*============================================================================*/
+/*============================================================================*/
+/*                    REUSE HISTORY - taken over from                         */
+/*============================================================================*/
+/*    Author         |      Version       |       Description                 */
+/*----------------------------------------------------------------------------*/
+/*Guillermo Hernandez|          1         |   Create Function to set the      */
+/*                   |                    |     state of the Indicator LEDS   */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*
+ * $Log: app_indicatorscontrol.c  $
+  ============================================================================*/
+
+/* Includes */
+/*============================================================================*/
+#include "app_indicatorscontrol.h"
+
+
+/* Constants and types  */
+/*============================================================================*/
+
+
+
+/* Variables */
+/*============================================================================*/
+
+
+
+/* Private functions prototypes */
+/*============================================================================*/
+
+
+
+/* Inline functions */
+/*============================================================================*/
+
+
+
+
+/* Private functions */
+/*============================================================================*/
+
+
+/* Exported functions */
+/*============================================================================*/
+void ic_void_Indicator_Up_ON(){
+	indicator_void_clear_indicator(PORTCH_D,UP_Indicator);
+}
+void ic_void_Indicator_Up_OFF(){
+	indicator_void_set_indicator(PORTCH_D,UP_Indicator);
+}
+
+void ic_void_Indicator_Down_ON(){
+	indicator_void_clear_indicator(PORTCH_D,DOWN_Indicator);
+}
+void ic_void_Indicator_Down_OFF(){
+	indicator_void_set_indicator(PORTCH_D,DOWN_Indicator);
+}
+
+
+
+
+
+ /* Notice: the file ends with a blank new line to avoid compiler warnings */
---
+++ Scheduler Window Lifter Code/Src/Bsw/App/app_indicatorscontrol.h
@@ -0,0 +1,67 @@
+/*============================================================================*/
+/*                        I BS SOFTWARE GROUP                                 */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*/
+/*!
+ * $Source: app_indicatorscontrol.h $
+ * $Revision: 1 $
+ * $Author: Hernandez Ramirez Guillermo, Hernandez Jimenez Manuel $
+ * $Date: 25/11/2017 $
+ */
+/*============================================================================*/
+/* DESCRIPTION :                                                              */
+/** \APP file for LED indicator control Prototype Functions.
+
+
+    
+*/
+/*============================================================================*/
+/* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
+/* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
+/* ALL RIGHTS RESERVED                                                        */
+/*                                                                            */
+/* The reproduction, transmission, or use of this document or its content is  */
+/* not permitted without express written authority. Offenders will be liable  */
+/* for damages.                                                               */
+/* All rights, including rights created by patent grant or registration of a  */
+/* utility model or design, are reserved.                                     */
+/*                                                                            */
+/*============================================================================*/
+/*============================================================================*/
+/*                    REUSE HISTORY - taken over from                         */
+/*============================================================================*/
+/*  AUTHOR             |    VERSION    |            DESCRIPTION               */
+/*----------------------------------------------------------------------------*/
+/*Guillermo Hernandez  |       1       |         Add Function Prototypes      */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*
+ * $Log: app_indicatorscontrol.h  $
+  ============================================================================*/
+#ifndef INDICATORSCONTROL_H
+#define INDICATORSCONTROL_H
+
+/* Includes */
+/*============================================================================*/
+#include "hal_indicators.h"
+
+/* Constants and types */
+/*============================================================================*/
+
+
+/* Exported Variables */
+/*============================================================================*/
+
+
+/* Exported functions prototypes */
+/*============================================================================*/
+void ic_void_Indicator_Up_ON();
+void ic_void_Indicator_Up_OFF();
+
+void ic_void_Indicator_Down_ON();
+void ic_void_Indicator_Down_OFF();
+
+
+#endif  /* Notice: the file ends with a blank new line to avoid compiler warnings */
