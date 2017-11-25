bcdc8abd36b42cdcfadb9c23165d270f7a99d9aa
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-11-25 14:21:55 -0600

    SchM_Types.h_Update_V13
    Changes and Updates on the files .
    Delete enums and structs used on the first version of the Scheduler
    File: SchM_Types.h

--- Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM_Types.h
+++ Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM_Types.h
@@ -1,25 +1,19 @@
-/*
- * SchM_Types.h
- *
- *  Created on: 15/11/2017
- *      Author: uid87753
- */
-
 /*============================================================================*/
 /*                        I BS SOFTWARE GROUP                                 */
 /*============================================================================*/
 /*                        OBJECT SPECIFICATION                                */
 /*============================================================================*/
 /*!
- * $Source: filename.x $
+ * $Source: SchM_Types.h $
  * $Revision: 1 $
- * $Author: Guillermo Hernández $
- * $Date: 11/17/2017 $
+ * $Author: Hernandez Ramirez Guillermo, Hernandez Jimenez Manuel $
+ * $Date: 25/11/2017 $
  */
 /*============================================================================*/
 /* DESCRIPTION :                                                              */
-/**
- */
+/** \
+		File with Enums and Struct of Task State & Task Status
+*/
 /*============================================================================*/
 /* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
 /* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
@@ -35,57 +29,83 @@
 /*============================================================================*/
 /*                    REUSE HISTORY - taken over from                         */
 /*============================================================================*/
-/*       AUTHOR        |      VERSION       |          DESCRIPTION            */
+/*  AUTHOR             |      VERSION       |        DESCRIPTION              */
 /*----------------------------------------------------------------------------*/
-/* Guillermo Hernández |           1        |        Update structures        */
-/*                     |                    |                                 */
-/* Manuel Hernández     |                    |                                 */
+/*Guillermo Hernandez  |           1        |  Adding comments                */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
 /*
- * $Log: filename.h  $
- ============================================================================*/
+ * $Log: SchM_Types.h  $
+  ============================================================================*/
+
 #ifndef BSW_SERVICES_SCHM_SCHM_TYPES_H_
 #define BSW_SERVICES_SCHM_SCHM_TYPES_H_
 
 /* Includes */
 /*============================================================================*/
-
 #include "Std_Types.h"
+
+
+/* Defines */
+/*============================================================================*/
+
+#define PINBKG      13 // Before: 9
+#define PIN3P125MS  7
+#define PIN6P25MS   17
+#define PIN12P5MS   14
+#define PIN25MS     15
+#define PIN50MS     16
+#define PIN100MS    3
+#define PINOVERLOAD 14 // Before: 16
+
 
 /* Constants and types */
 /*============================================================================*/
 
-#define LED_BKG      0
-#define LED_3P125MS  7
-#define LED_6P25MS   17
-#define LED_12P5MS   14
-#define LED_25MS     15
-#define LED_50MS     16
-#define LED_100MS    14
-#define LED_OVERLOAD 15
+typedef enum
+{
+	SCHM_TASK_STATE_SUSPENDED,
+	SCHM_TASK_STATE_READY,
+	SCHM_TASK_STATE_RUNNING
+}SchM_TaskStateType;
+
+typedef enum
+{
+	SCHM_UNINIT,
+	SCHM_INIT,
+	SCHM_IDLE,
+	SCHM_RUNNING,
+	SCHM_OVERLOAD,
+	SCHM_HALTED
+}SchM_SchedulerStateType;
+
+typedef struct
+{
+	SchM_TaskStateType SchM_TaskState;
+}SchM_TaskControlBlockType;
+
+typedef struct
+{
+	uint16_t	OsTickCounter;
+	SchM_SchedulerStateType SchM_SchedulerState;
+}SchM_SchedulerStatusType;
+
 
 /* Exported Variables */
 /*============================================================================*/
 
+
 /* Exported functions prototypes */
 /*============================================================================*/
-typedef enum {
-	SCHM_TASK_STATE_SUSPENDED, SCHM_TASK_STATE_READY, SCHM_TASK_STATE_RUNNING
-} SchM_TaskStateType;
 
-typedef enum {
-	SCHM_UNINIT, SCHM_INIT, SCHM_IDLE, SCHM_RUNNING, SCHM_OVERLOAD, SCHM_HALTED
-} SchM_SchedulerStateType;
-
-typedef struct {
-	SchM_TaskStateType SchM_TaskState;
-} SchM_TaskControlBlockType;
-
-typedef struct {
-	uint16_t OsTickCounter;
-	SchM_SchedulerStateType SchM_SchedulerState;
-} SchM_SchedulerStatusType;
 
 #endif /* BSW_SERVICES_SCHM_SCHM_TYPES_H_ */
+
+/* Notice: the file ends with a blank new line to avoid compiler warnings */
+
+
+
+
+
+
