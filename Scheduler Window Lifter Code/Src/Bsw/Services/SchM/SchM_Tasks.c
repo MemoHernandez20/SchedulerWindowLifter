b315d2788ccff7f8306137188fae6975b7f89950
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-11-25 22:26:21 -0600

    SchM_Task_Update_V24
    Add Logical Structures on state #5 & state#6 (Case 5 & Case6)

--- Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM_Tasks.c
+++ Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM_Tasks.c
@@ -5,7 +5,7 @@
 /*============================================================================*/
 /*!
  * $Source: SchM_Tasks.c $
- * $Revision: 8 $
+ * $Revision: 9 $
  * $Author: Hernandez Ramirez Guillermo, Hernandez Jimenez Manuel $
  * $Date: 25/11/2017 $
  */
@@ -56,6 +56,9 @@
 /*----------------------------------------------------------------------------*/
 /*Guillermo Hernandez  |           8        | Add Logical Structures on state */
 /*                     |                    |                #4               */
+/*----------------------------------------------------------------------------*/
+/*Guillermo Hernandez  |           9        | Add Logical Structures on state */
+/*                     |                    |                #5 and #6        */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
@@ -242,22 +245,22 @@
 
 		/* Conditions */
 
-		if(rs_Flag.bi1_flagAutomaticUp&&rub_level==10){ /* Conditions of transitions between states 4 -> 0 */
-			/* Action to enable transition between state 4 - 0 */
+		if(rs_Flag.bi1_flagAutomaticUp&&rub_level==10){ /* Conditions of transitions between states 4 -> 1 */
+			/* Action to enable transition between state 4 - 1 */
 			state=1;
 			rs_Flag.bi1_flagAutomaticUp=0;
 			rs_Flag.bi1_flagUp=0;
 		}
 
-		if(rs_Flag.bi1_flagAutomaticDown&&rub_level==0){ /* Conditions of transitions between states 4 -> 0 */
-			/* Action to enable transition between state 4 - 0 */
+		if(rs_Flag.bi1_flagAutomaticDown&&rub_level==0){ /* Conditions of transitions between states 4 -> 1 */
+			/* Action to enable transition between state 4 - 1 */
 			state=1;
 			rs_Flag.bi1_flagAutomaticDown=0;
 			rs_Flag.bi1_flagDown=0;
 		}
 
-		if((bc_T_UBYTE_UP_Button())||(bc_T_UBYTE_DOWN_Button())){ /* Conditions of transitions between states 4 -> 1 */
-			/* Action to enable transition between state 4 - 1 */
+		if((bc_T_UBYTE_UP_Button())||(bc_T_UBYTE_DOWN_Button())){ /* Conditions of transitions between states 4 -> 2 */
+			/* Action to enable transition between state 4 - 2 */
 			state=2;
 			rs_Flag.bi1_flagAutomaticDown=0;
 			rs_Flag.bi1_flagDown=0;
@@ -266,8 +269,8 @@
 			ruw_lpit0_counter=0;
 		}
 
-		if((rs_Flag.bi1_flagUp)&&bc_T_UBYTE_ANTIPINCH_Button()){ /* Conditions of transitions between states 4 -> 1 */
-			/* Action to enable transition between state 4 - 1 */
+		if((rs_Flag.bi1_flagUp)&&bc_T_UBYTE_ANTIPINCH_Button()){ /* Conditions of transitions between states 4 -> 2 */
+			/* Action to enable transition between state 4 - 2 */
 			state=2;
 			rs_Flag.bi1_flagAutomaticUp=0;
 			rs_Flag.bi1_flagUp=0;
@@ -282,6 +285,79 @@
 	/*============================================================================================================================*/
 	case 5:
 	
+			/* Actions state 5 */
+		if(rs_Flag.bi1_flagManualDown){
+			if(rub_level>=1){
+				if(bc_T_UBYTE_DOWN_Button()){
+					if(ruw_lpit0_counter>=_400ms){
+						wc_void_DOWN_Mode_Routine (&rub_level);
+						rub_level--;
+						ruw_lpit0_counter=0;
+				}
+
+					}
+
+			}
+
+
+		}
+
+		if(rs_Flag.bi1_flagManualUp){
+			if(rub_level<10){
+				if(bc_T_UBYTE_UP_Button()){
+					if(ruw_lpit0_counter>=_400ms){
+						rub_level++;
+						wc_void_UP_Mode_Routine (&rub_level);
+						ruw_lpit0_counter=0;
+					}
+
+				}
+
+
+			}
+
+
+		}
+
+		/* Conditions */
+
+		if(rs_Flag.bi1_flagManualUp&&rub_level==10){ /* Conditions of transitions between states 5 -> 1 */
+			/* Action to enable transition between state 5 - 1 */
+			state=1;
+			rs_Flag.bi1_flagManualUp=0;
+			rs_Flag.bi1_flagUp=0;
+		}
+
+		if(rs_Flag.bi1_flagManualDown&&rub_level==0){ /* Conditions of transitions between states 5 -> 1 */
+			/* Action to enable transition between state 5 - 1 */
+			state=1;
+			rs_Flag.bi1_flagManualDown=0;
+			rs_Flag.bi1_flagDown=0;
+		}
+
+		if((bc_T_UBYTE_UP_Button()==0)&&(rs_Flag.bi1_flagManualUp)){ /* Conditions of transitions between states 5 -> 1 */
+			/* Action to enable transition between state 5 - 1 */
+			state=1;
+			rs_Flag.bi1_flagManualUp=0;
+			rs_Flag.bi1_flagUp=0;
+		}
+
+		if((bc_T_UBYTE_DOWN_Button()==0)&&(rs_Flag.bi1_flagManualDown)){ /* Conditions of transitions between states 5 -> 1 */
+			/* Action to enable transition between state 5 - 1 */
+			state=1;
+			rs_Flag.bi1_flagManualDown=0;
+			rs_Flag.bi1_flagDown=0;
+
+		}
+
+		if((rs_Flag.bi1_flagUp)&&bc_T_UBYTE_ANTIPINCH_Button()){ /* Conditions of transitions between states 5 -> 2 */
+			/* Action to enable transition between state 5 - 2 */
+			state=2;
+			rs_Flag.bi1_flagManualUp=0;
+			rs_Flag.bi1_flagUp=0;
+			ruw_lpit0_counter=0;
+		}
+		
 		break;
 	/*============================================================================================================================*/
 
@@ -289,7 +365,32 @@
 	/*============================================================================================================================*/
 	case 6:
 	    
+			 /* Actions state 6 */
+		if(rs_Flag.bi1_FlagAntipinch){
+			if(rub_level>=1){
+				if(ruw_lpit0_counter>=_400ms){
+					wc_void_DOWN_Mode_Routine(&rub_level);
+					rub_level--;
+					ruw_lpit0_counter=0;
+				}
 
+			}
+
+		}
+
+	     /* Conditions */
+		if(rs_Flag.bi1_FlagAntipinch){ /* Condition to stay on state 6  */
+			/* Actions to stay on state 6 */
+			state=6;
+		}
+
+		if((rs_Flag.bi1_FlagAntipinch)&&rub_level==0){ /* Conditions of transitions between states 6 -> 7 */
+			/* Action to enable transition between state 6 - 7 */
+			state=7;
+			rs_Flag.bi1_FlagAntipinch=0;
+			rs_Flag.bi1_flagWaiting5Secons=1;
+			ruw_lpit0_counter=0;
+		}
 
 		break;
 	/*============================================================================================================================*/
