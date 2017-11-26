0064d0fbd20d00d8d4eedf297c7ca26d28d5c598
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-11-26 14:49:24 -0600

    Revert "Buttons_Update_V30"
    This reverts commit 5f768695aa8ae93bbe9b90637c8a1483ea603fbd.

--- Scheduler Window Lifter Code/Src/Bsw/Hal/hal_indicators.c
+++ Scheduler Window Lifter Code/Src/Bsw/Hal/hal_indicators.c
@@ -5,15 +5,15 @@
 /*============================================================================*/
 /*!
  * $Source: indicators.c $
- * $Revision: 2 $
+ * $Revision: 1 $
  * $Author: Hernandez Ramirez Guillermo, Hernandez Jimenez Manuel  $
- * $Date: 26/11/17 $
+ * $Date: 25/10/17 $
  */
 /*============================================================================*/
 /* DESCRIPTION :                                                              */
 /** \file
- HAL file to change the state of the indicators
- */
+    HAL file to change the state of the indicators
+*/
 /*============================================================================*/
 /* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
 /* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
@@ -33,43 +33,55 @@
 /*----------------------------------------------------------------------------*/
 /*Guillermo Hernandez  |         1          |  Functions to change the state  */
 /*                     |                    |         of the Indicator LEDS   */
-/*----------------------------------------------------------------------------*/
-/*Guillermo Hernandez  |         2          |    Add Format to the Files      */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
 /*
  * $Log: indicators.c  $
- ============================================================================*/
+  ============================================================================*/
 
 /* Includes */
 /*============================================================================*/
 #include "hal_indicators.h"
 
+
+
 /* Constants and types  */
 /*============================================================================*/
+
+
 
 /* Variables */
 /*============================================================================*/
 
+
+
 /* Private functions prototypes */
 /*============================================================================*/
+
+
 
 /* Inline functions */
 /*============================================================================*/
 
+
+
+
 /* Private functions */
 /*============================================================================*/
+
+
 
 /* Exported functions */
 /*============================================================================*/
 
-void indicator_void_set_indicator(DioPortType DioPort, uint32_t DioPin) {
-	Dio_PortSetPin(DioPort, DioPin);
+void indicator_void_set_indicator(DioPortType DioPort, uint32_t DioPin){
+	Dio_PortSetPin(DioPort,DioPin);
 }
 
-void indicator_void_clear_indicator(DioPortType DioPort, uint32_t DioPin) {
-	Dio_PortClearPin(DioPort, DioPin);
+void indicator_void_clear_indicator(DioPortType DioPort, uint32_t DioPin){
+	Dio_PortClearPin(DioPort,DioPin);
 }
 
-/* Notice: the file ends with a blank new line to avoid compiler warnings */
+
+ /* Notice: the file ends with a blank new line to avoid compiler warnings */
