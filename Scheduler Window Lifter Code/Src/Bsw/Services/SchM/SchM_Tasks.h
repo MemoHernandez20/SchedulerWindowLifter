c9fce55ad96932832e7019d81a161191d60414f5
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-11-25 20:49:30 -0600

    SchM_Task_Update_V15
    Define BItFields used as Flags.
    Files: SchM_Task.h

--- Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM_Tasks.h
+++ Scheduler Window Lifter Code/Src/Bsw/Services/SchM/SchM_Tasks.h
@@ -1,25 +1,22 @@
-/*
- * SchM_Tasks.h
- *
- *  Created on: 15/11/2017
- *      Author: uid87753
- */
-
 /*============================================================================*/
 /*                        I BS SOFTWARE GROUP                                 */
 /*============================================================================*/
 /*                        OBJECT SPECIFICATION                                */
 /*============================================================================*/
 /*!
- * $Source: filename.x $
+ * $Source: SchM_Tasks.h $
  * $Revision: 1 $
- * $Author: Guillermo Hernández $
- * $Date: 11/17/2017 $
+ * $Author: Hernandez Ramirez Guillermo, Hernandez Jimenez Manuel $
+ * $Date: 25/11/2017 $
  */
 /*============================================================================*/
 /* DESCRIPTION :                                                              */
-/**
- */
+/** \
+		SchM_Task.h file
+		Definition of flags.
+		Definition of Task Prototypes.
+		
+*/
 /*============================================================================*/
 /* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
 /* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
@@ -35,48 +32,101 @@
 /*============================================================================*/
 /*                    REUSE HISTORY - taken over from                         */
 /*============================================================================*/
-/*       AUTHOR        |      VERSION       |          DESCRIPTION            */
+/*  AUTHOR             |      VERSION       |        DESCRIPTION              */
 /*----------------------------------------------------------------------------*/
-/* Guillermo Hernández |           1        |        Update structures        */
+/*Guillermo Hernandez  |          1         | Create a bitfield used as flags */
 /*                     |                    |                                 */
-/* Manuel Hernández     |                    |                                 */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
 /*
- * $Log: filename.h  $
- ============================================================================*/
+ * $Log: SchM_Tasks.h  $
+  ============================================================================*/
+
 #ifndef BSW_SERVICES_SCHM_SCHM_TASKS_H_
 #define BSW_SERVICES_SCHM_SCHM_TASKS_H_
 
 /* Includes */
 /*============================================================================*/
-
 #include "Std_Types.h"
+#include "app_buttonscontrol.h"
+#include "app_indicatorscontrol.h"
+#include "app_windowcontrol.h"
+#include "Dio.h"
+
+
+/* Defines */
+/*============================================================================*/
+
+#define NumberOfCycles (uint32_t)2700
+
+#define WINDOW_LED1        1
+#define WINDOW_LED2        14
+#define WINDOW_LED3        14
+#define WINDOW_LED4        15
+#define WINDOW_LED5        16
+#define WINDOW_LED6        16
+#define WINDOW_LED7        15
+#define WINDOW_LED8        14
+#define WINDOW_LED9        17
+#define WINDOW_LED10       7
+
+#define BUTTON_UP          12
+#define BUTTON_DOWN        13
+#define BUTTON_ANTIPINCH   7
+
+#define INDICATOR_UP       0
+#define INDICATOR_DOWN     16
+
+#define TASK_1MS          1
+#define TASK_2MS          3
+#define TRANSITION_400MS  3
+
+
+#define _1ms 1
+#define _10ms 10
+#define _500ms 500
+#define _400ms 400
+#define _1000ms 1000
+#define _3000ms 3000
+#define _5000ms 5000
+
 
 /* Constants and types */
 /*============================================================================*/
-#define NumberOfCycles (uint32_t)0
+
+typedef struct {
+
+	T_UBYTE bi1_flagAutomaticUp:1;
+	T_UBYTE bi1_flagAutomaticDown:1;
+	T_UBYTE bi1_flagManualUp:1;
+	T_UBYTE bi1_flagManualDown:1;
+	T_UBYTE bi1_flagUp:1;
+	T_UBYTE bi1_flagDown:1;
+	T_UBYTE bi1_FlagAntipinch:1;
+	T_UBYTE bi1_flagWaiting5Secons:1;
+
+}S_Structflags;
+
+
+T_UWORD ruw_lpit0_counter; /* LPIT0 chan 0 timeout counter */
+T_UBYTE rub_level;
+S_Structflags rs_Flag;
+
+
 
 /* Exported Variables */
 /*============================================================================*/
 
+
 /* Exported functions prototypes */
 /*============================================================================*/
-#define LED_BKG      0
-#define LED_3P125MS  7
-#define LED_6P25MS   17
-#define LED_12P5MS   14
-#define LED_25MS     15
-#define LED_50MS     16
-#define LED_100MS    14
-#define LED_OVERLOAD 15
 
-extern void SchM_3p125ms_Task(void);
-extern void SchM_6p25ms_Task(void);
-extern void SchM_12p5ms_Task(void);
-extern void SchM_25ms_Task(void);
-extern void SchM_50ms_Task(void);
-extern void SchM_100ms_Task(void);
+extern void SchM_1MS_Task  ( void );
+extern void SchM_2MS_Task ( void );
 
 #endif /* BSW_SERVICES_SCHM_SCHM_TASKS_H_ */
+ 
+
+/* Notice: the file ends with a blank new line to avoid compiler warnings */
+
